f0a6ff68545b7c08c314a3f57547e58b804d7c5b
hemantk <hemantk@microsoft.com>
2020-11-05 15:10:25 +0530

    added blank projects for retailer and bdd

---
+++ blendnet.crm.contentprovider.api/blendnet.crm.retailer.api/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace blendnet.crm.retailer.api.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
