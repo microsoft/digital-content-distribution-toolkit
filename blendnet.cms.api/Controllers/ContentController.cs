84a8df5888a3f5d62f74c94bc0b5ad1b70ea7b53
hemantk <hemantk@microsoft.com>
2021-03-25 18:46:07 +0530

    Fixed string concatenation issue

--- blendnet.cms.api/Controllers/ContentController.cs
+++ blendnet.cms.api/Controllers/ContentController.cs
@@ -420,19 +420,17 @@ namespace blendnet.cms.api.Controllers
 
         private async Task<List<string>> ValidateBlobExistence(List<Content> contents,Guid contentProviderId)
         {
-            var rawcontainerName = $"{contentProviderId}ApplicationConstants.StorageContainerSuffix.Raw"; 
+            var rawcontainerName = $"{contentProviderId}{ApplicationConstants.StorageContainerSuffix.Raw}"; 
 
             BlobContainerClient rawClient = _cmsBlobServiceClient.GetBlobContainerClient(rawcontainerName);
-
-            List<String> Mediatypes = ApplicationConstants.SupportedFileFormats.mediaFormats;
-                
-            List<String> Thumbnailtypes = ApplicationConstants.SupportedFileFormats.ThumbnailFormats;
 
             List<string> errorList = new List<string>();
             
             foreach(Content content in contents)
-            {         
-                if(! await rawClient.GetBlobClient(content.MediaFileName).ExistsAsync())
+            {
+                bool exists = rawClient.GetBlobClient(content.MediaFileName).Exists();
+
+                if (!await rawClient.GetBlobClient(content.MediaFileName).ExistsAsync())
                 {
                     string info = $"Blob file of type Media with name {content.MediaFileName} from the content {content.Title} is not found.";
 
