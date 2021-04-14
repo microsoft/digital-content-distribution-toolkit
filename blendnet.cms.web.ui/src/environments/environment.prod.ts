8771d91a176089d9c846b503795d8ebbedabdaca
Kishori Patil <kipati@microsoft.com>
2021-04-14 14:13:51 +0530

    added env to prod and removed proxy config

--- blendnet.cms.web.ui/src/environments/environment.prod.ts
+++ blendnet.cms.web.ui/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  maxFileUploadSize: 1000000,
+  fileAllowedType: 'application/json',
+  contentProviderApiUrl: 'https://localhost:44338/api/v1/ContentProvider',
+  contentApiUrl: 'https://localhost:44338/api/v1/Content',
+  userApiUrl: 'https://localhost:44397/api/v1/Identity'
 };
