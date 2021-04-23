d380bcb9941666213d5e51564f1ab33221b337c9
Sameer <sameera@microsoft.com>
2021-04-23 18:12:18 +0530

    Move subscription under ContentProvider namespace

--- blendnet.cms.api/Controllers/SubscriptionController.cs
+++ blendnet.cms.api/Controllers/SubscriptionController.cs
@@ -13,7 +13,7 @@ namespace blendnet.cms.api.Controllers
     /// <summary>
     /// Controller for managing Subscription metadata
     /// </summary>
-    [Route("api/v{version:apiVersion}/[controller]")]
+    [Route("api/v{version:apiVersion}/ContentProvider/{contentProviderId:guid}/[controller]")]
     [ApiVersion("1.0")]
     [ApiController]
     public class SubscriptionController : Controller
@@ -35,33 +35,112 @@ namespace blendnet.cms.api.Controllers
         /// </summary>
         /// <param name="contentProviderId">Content Provider ID</param>
         /// <returns></returns>
-        [HttpGet("{contentProviderId:guid}")]
+        [HttpGet]
         public async Task<ActionResult<List<ContentProviderSubscriptionDto>>> GetSubscriptions(Guid contentProviderId)
         {
             var result = await this._contentProviderRepository.GetSubscriptions(contentProviderId);
             return Ok(result);
         }
 
+        [HttpGet]
+        public async Task<ActionResult<List<ContentProviderSubscriptionDto>>> GetSubscription(Guid contentProviderId, Guid subscriptionId)
+        {
+            var result = await this._contentProviderRepository.GetSubscription(contentProviderId, subscriptionId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
         /// <summary>
         /// Creates a new subscription
         /// </summary>
-        /// <param name="subscriptionMetadata">subscription data</param>
+        /// <param name="subscription">subscription data</param>
         /// <returns>ID of the created subscription</returns>
-        [HttpPost("{contentProviderId:guid}")]
+        [HttpPost]
         public async Task<ActionResult<String>> CreateSubscription( Guid contentProviderId,
-                                                                    ContentProviderSubscriptionDto subscriptionMetadata)
+                                                                    ContentProviderSubscriptionDto subscription)
         {
             if (!await ValidContentProvider(contentProviderId))
             {
                 return NotFound();
             }
 
-            subscriptionMetadata.SetIdentifiers();
-            subscriptionMetadata.ContentProviderId = contentProviderId;
-            subscriptionMetadata.Type = ContentProviderContainerType.SubscriptionMetadata;
+            subscription.SetIdentifiers();
+            subscription.ContentProviderId = contentProviderId;
+            subscription.Type = ContentProviderContainerType.SubscriptionMetadata;
 
-            var subscriptionId = await _contentProviderRepository.CreateSubscription(subscriptionMetadata);
+            var subscriptionId = await this._contentProviderRepository.CreateSubscription(subscription);
             return Ok(subscriptionId);
+        }
+
+        /// <summary>
+        /// Update a subscription
+        /// </summary>
+        /// <param name="contentProviderId">contentProvider ID</param>
+        /// <param name="subscriptionId">subscription ID</param>
+        /// <param name="subscription">new subscription data</param>
+        /// <returns></returns>
+        [HttpPut("{subscriptionId:guid}")]
+        public async Task<ActionResult<String>> UpdateSubscription( Guid contentProviderId,
+                                                                    Guid subscriptionId,
+                                                                    ContentProviderSubscriptionDto subscription)
+        {
+            if (!await ValidContentProvider(contentProviderId))
+            {
+                return NotFound();
+            }
+
+            // Update is allowed only if there are no orders against this subscription
+            if (await OrdersExistForSubscription(subscriptionId))
+            {
+                return Conflict();
+            }
+
+            subscription.Id = subscriptionId;
+            subscription.ContentProviderId = contentProviderId;
+            subscription.Type = ContentProviderContainerType.SubscriptionMetadata;
+
+            int response = await this._contentProviderRepository.UpdateSubscription(contentProviderId, subscription);
+            if (response == (int)System.Net.HttpStatusCode.OK)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+
+        /// <summary>
+        /// Delete a subscription
+        /// </summary>
+        /// <param name="contentProviderId">contentProvider ID</param>
+        /// <param name="subscriptionId">subscription ID</param>
+        /// <returns></returns>
+        [HttpDelete("{subscriptionId:guid}")]
+        public async Task<ActionResult<String>> DeleteSubscription( Guid contentProviderId,
+                                                                    Guid subscriptionId)
+        {
+            // Delete is allowed only if there are no orders against this subscription
+            if (await OrdersExistForSubscription(subscriptionId))
+            {
+                return Conflict();
+            }
+
+            var response = await this._contentProviderRepository.DeleteSubscription(contentProviderId, subscriptionId);
+            if (response == (int)System.Net.HttpStatusCode.NoContent)
+            {
+                return NoContent();
+            }
+            else
+            {
+                return NotFound();
+            }
         }
 
         #endregion
@@ -77,6 +156,11 @@ namespace blendnet.cms.api.Controllers
         {
             var contentProvider = await this._contentProviderRepository.GetContentProviderById(contentProviderId);
             return contentProvider != default(ContentProviderDto);
+        }
+
+        private Task<bool> OrdersExistForSubscription(Guid subscriptionId)
+        {
+            return Task.FromResult(false);
         }
 
         #endregion
