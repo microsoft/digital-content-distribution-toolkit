eef434926f83869973a46aaa6c6a9cc81bdcd244
hemantk <hemantk@microsoft.com>
2021-04-29 12:35:17 +0530

    Moved token generation to common utilities

--- blendnet.cms.api/Common/AmsHelper.cs
+++ blendnet.cms.api/Common/AmsHelper.cs
@@ -39,97 +39,23 @@ namespace blendnet.cms.api.Common
         /// <returns></returns>
         public async Task<string> GetContentToken(Guid contentId, Guid commandId)
         {
-            IAzureMediaServicesClient amsclient = await GetAmsClient();
+            AmsData amsData = new AmsData();
 
-            string token = string.Empty;
+            amsData.AmsAccountName = _appSettings.AmsAccountName;
+            amsData.AmsArmEndPoint = _appSettings.AmsArmEndPoint;
+            amsData.AmsClientId = _appSettings.AmsClientId;
+            amsData.AmsClientSecret = _appSettings.AmsClientSecret;
+            amsData.AmsResourceGroupName = _appSettings.AmsResourceGroupName;
+            amsData.AmsSubscriptionId = _appSettings.AmsSubscriptionId;
+            amsData.AmsTenantId = _appSettings.AmsTenantId;
+            amsData.AmsTokenAudience = _appSettings.AmsTokenAudience;
+            amsData.AmsTokenExpiryInMts = _appSettings.AmsTokenExpiryInMts;
+            amsData.AmsTokenIssuer = _appSettings.AmsTokenIssuer;
+            amsData.AmsTokenSigningKey = _appSettings.AmsTokenSigningKey;
 
-            string locatorName = $"{contentId}|{commandId}";
-
-            byte[] tokenSigningKey = Convert.FromBase64String(_appSettings.AmsTokenSigningKey);
-
-            // Get Streaming locator
-            StreamingLocator locator = await amsclient.StreamingLocators.GetAsync(_appSettings.AmsResourceGroupName, 
-                                                                                  _appSettings.AmsAccountName,
-                                                                                  locatorName);
-
-            if (locator != null)
-            {
-                // In this example, we want to play the Widevine (CENC) encrypted stream. 
-                // We need to get the key identifier of the content key where its type is CommonEncryptionCenc.
-                string keyIdentifier = locator.ContentKeys.Where(k => k.Type == StreamingLocatorContentKeyType.CommonEncryptionCenc).First().Id.ToString();
-
-                // In order to generate our test token we must get the ContentKeyId to put in the ContentKeyIdentifierClaim claim.
-                token = GenerateTokenAsync(_appSettings.AmsTokenIssuer, 
-                                            _appSettings.AmsTokenAudience,
-                                            keyIdentifier,
-                                            _appSettings.AmsTokenExpiryInMts,
-                                            tokenSigningKey);
-            }
-            else
-            {
-                _logger.LogInformation($"No streaming locator found for {locatorName}");
-            }
+            string token = await AmsUtilities.GetContentToken(amsData, contentId, commandId);
 
             return token;
-        }
-
-
-
-        /// <summary>
-        /// Create a token that will be used to protect your stream.
-        /// Only authorized clients would be able to play the video.  
-        /// </summary>
-        /// <param name="issuer">The issuer is the secure token service that issues the token. </param>
-        /// <param name="audience">The audience, sometimes called scope, describes the intent of the token or the resource the token authorizes access to. </param>
-        /// <param name="keyIdentifier">The content key ID.</param>
-        /// <param name="tokenVerificationKey">Contains the key that the token was signed with. </param>
-        /// <returns></returns>
-        private string GenerateTokenAsync(string issuer, 
-                                            string audience, 
-                                            string keyIdentifier, 
-                                            int tokenExpiryInMts,
-                                            byte[] tokenVerificationKey)
-        {
-            var tokenSigningKey = new SymmetricSecurityKey(tokenVerificationKey);
-
-            SigningCredentials cred = new SigningCredentials(
-                tokenSigningKey,
-                // Use the  HmacSha256 and not the HmacSha256Signature option, or the token will not work!
-                SecurityAlgorithms.HmacSha256,
-                SecurityAlgorithms.Sha256Digest);
-
-            Claim[] claims = new Claim[]
-            {
-                new Claim(ContentKeyPolicyTokenClaim.ContentKeyIdentifierClaim.ClaimType, keyIdentifier)
-            };
-
-            JwtSecurityToken token = new JwtSecurityToken(
-                issuer: issuer,
-                audience: audience,
-                claims: claims,
-                notBefore: DateTime.Now.AddMinutes(-5),
-                expires: DateTime.Now.AddMinutes(tokenExpiryInMts),
-                signingCredentials: cred);
-
-            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
-
-            return handler.WriteToken(token);
-        }
-
-        /// <summary>
-        /// Gets the AMS Client
-        /// </summary>
-        /// <returns></returns>
-        private async Task<IAzureMediaServicesClient> GetAmsClient()
-        {
-            IAzureMediaServicesClient amsclient = await AmsUtilities.CreateMediaServicesClientAsync(_appSettings.AmsArmEndPoint,
-                                                                                                     _appSettings.AmsClientId,
-                                                                                                     _appSettings.AmsClientSecret,
-                                                                                                     _appSettings.AmsTenantId,
-                                                                                                    _appSettings.AmsSubscriptionId);
-
-            return amsclient;
-
         }
 
     }
--- blendnet.common.infrastructure/Ams/AmsUtilities.cs
+++ blendnet.common.infrastructure/Ams/AmsUtilities.cs
@@ -1,11 +1,15 @@
 ﻿using Microsoft.Azure.Management.Media;
+using Microsoft.Azure.Management.Media.Models;
 using Microsoft.IdentityModel.Clients.ActiveDirectory;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.Rest;
 using Microsoft.Rest.Azure.Authentication;
 using System;
-using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace blendnet.common.infrastructure.Ams
 {
@@ -17,7 +21,7 @@ namespace blendnet.common.infrastructure.Ams
         /// </summary>
         /// <param name="config">The param is of type ConfigWrapper. This class reads values from local configuration file.</param>
         /// <returns></returns>
-        public static async Task<IAzureMediaServicesClient> CreateMediaServicesClientAsync( string amsArmEndPoint, 
+        private static async Task<IAzureMediaServicesClient> CreateMediaServicesClientAsync( string amsArmEndPoint, 
                                                                                             string amsClientId,
                                                                                             string amsClientSecret,
                                                                                             string amsTenantId,
@@ -44,5 +48,117 @@ namespace blendnet.common.infrastructure.Ams
                                                                     clientCredential, 
                                                                     ActiveDirectoryServiceSettings.Azure);
         }
+
+        /// <summary>
+        /// Generates the token for the Content and Command Id
+        /// </summary>
+        /// <param name="contentId"></param>
+        /// <param name="commandId"></param>
+        /// <returns></returns>
+        public static async Task<string> GetContentToken(AmsData amsData,
+                                                         Guid contentId, 
+                                                         Guid commandId)
+        {
+            IAzureMediaServicesClient amsclient = await CreateMediaServicesClientAsync(amsData.AmsArmEndPoint,
+                                                                                        amsData.AmsClientId,
+                                                                                        amsData.AmsClientSecret,
+                                                                                        amsData.AmsTenantId,
+                                                                                        amsData.AmsSubscriptionId);
+
+            string token = string.Empty;
+
+            string locatorName = $"{contentId}|{commandId}";
+
+            byte[] tokenSigningKey = Convert.FromBase64String(amsData.AmsTokenSigningKey);
+
+            // Get Streaming locator
+            StreamingLocator locator = await amsclient.StreamingLocators.GetAsync(amsData.AmsResourceGroupName,
+                                                                                  amsData.AmsAccountName,
+                                                                                  locatorName);
+
+            if (locator != null)
+            {
+                // In this example, we want to play the Widevine (CENC) encrypted stream. 
+                // We need to get the key identifier of the content key where its type is CommonEncryptionCenc.
+                string keyIdentifier = locator.ContentKeys.Where(k => k.Type == StreamingLocatorContentKeyType.CommonEncryptionCenc).First().Id.ToString();
+
+                // In order to generate our test token we must get the ContentKeyId to put in the ContentKeyIdentifierClaim claim.
+                token = GenerateTokenAsync(amsData.AmsTokenIssuer,
+                                           amsData.AmsTokenAudience,
+                                           keyIdentifier,
+                                           amsData.AmsTokenExpiryInMts,
+                                           tokenSigningKey);
+            }
+            
+            return token;
+        }
+
+
+
+        // <summary>
+        /// Create a token that will be used to protect your stream.
+        /// Only authorized clients would be able to play the video.  
+        /// </summary>
+        /// <param name="issuer">The issuer is the secure token service that issues the token. </param>
+        /// <param name="audience">The audience, sometimes called scope, describes the intent of the token or the resource the token authorizes access to. </param>
+        /// <param name="keyIdentifier">The content key ID.</param>
+        /// <param name="tokenVerificationKey">Contains the key that the token was signed with. </param>
+        /// <returns></returns>
+        private static string GenerateTokenAsync(string issuer,
+                                            string audience,
+                                            string keyIdentifier,
+                                            int tokenExpiryInMts,
+                                            byte[] tokenVerificationKey)
+        {
+            var tokenSigningKey = new SymmetricSecurityKey(tokenVerificationKey);
+
+            SigningCredentials cred = new SigningCredentials(
+                tokenSigningKey,
+                // Use the  HmacSha256 and not the HmacSha256Signature option, or the token will not work!
+                SecurityAlgorithms.HmacSha256,
+                SecurityAlgorithms.Sha256Digest);
+
+            Claim[] claims = new Claim[]
+            {
+                new Claim(ContentKeyPolicyTokenClaim.ContentKeyIdentifierClaim.ClaimType, keyIdentifier)
+            };
+
+            JwtSecurityToken token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                notBefore: DateTime.Now.AddMinutes(-5),
+                expires: DateTime.Now.AddMinutes(tokenExpiryInMts),
+                signingCredentials: cred);
+
+            JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+
+            return handler.WriteToken(token);
+        }
+    }
+
+    public class AmsData
+    {
+        public string AmsArmEndPoint { get; set; }
+        
+        public string AmsClientId { get; set; }
+        
+        public string AmsClientSecret { get; set; }
+        
+        public string AmsTenantId { get; set; }
+        
+        public string AmsSubscriptionId { get; set; }
+        
+        public string AmsTokenSigningKey { get; set; }
+        
+        public string AmsResourceGroupName { get; set; }
+        
+        public string AmsAccountName { get; set; }
+        
+        public string AmsTokenIssuer { get; set; }
+        
+        public string AmsTokenAudience { get; set; }
+        
+        public int AmsTokenExpiryInMts { get; set; }
     }
 }
