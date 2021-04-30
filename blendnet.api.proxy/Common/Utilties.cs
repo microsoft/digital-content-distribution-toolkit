b4d9c7e837a25b4186c99580c126c8a4fd8a1b8b
hemantk <hemantk@microsoft.com>
2021-04-30 18:45:18 +0530

    Identity First Version

---
+++ blendnet.api.proxy/Common/Utilties.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace blendnet.api.proxy.Common
+{
+    public class Utilties
+    {
+        /// <summary>
+        /// GetJsonSerializerOptions
+        /// </summary>
+        /// <returns></returns>
+        public static JsonSerializerOptions GetJsonSerializerOptions()
+        {
+            JsonSerializerOptions _jsonSerializerOptions = new JsonSerializerOptions() { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+            _jsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+            return _jsonSerializerOptions;
+        }
+    }
+}
---
+++ blendnet.common.dto/Identity/ValidatePartnerAccessTokenResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blendnet.common.dto.Identity
+{
+    public class ValidatePartnerAccessTokenResponse
+    {
+        public string UserRole { get; set; }
+
+        public KiazalaCredentials KiazalaCredentials { get; set; }
+
+        public string UID { get; set; }
+    }
+}
