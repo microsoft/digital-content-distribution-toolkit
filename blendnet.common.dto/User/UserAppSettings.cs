638953c2e6b06dbc181d333cdcc6bcdb96abd955
hemantk <hemantk@microsoft.com>
2021-05-03 15:56:24 +0530

    Added User Project

---
+++ blendnet.common.dto/User/UserAppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blendnet.common.dto.User
+{
+    public class UserAppSettings
+    {
+        public string AccountEndPoint { get; set; }
+
+        public string AccountKey { get; set; }
+
+        public string DatabaseName { get; set; }
+    }
+}
