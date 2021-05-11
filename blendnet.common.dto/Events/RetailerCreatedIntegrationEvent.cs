3a587746c20c054d9495e61d6ad58cd74fb52133
hemantk <hemantk@microsoft.com>
2021-05-11 14:02:40 +0530

    Added Retailer Listener and  Minor Modification to CMS Listener

---
+++ blendnet.common.dto/Events/RetailerCreatedIntegrationEvent.cs
@@ -0,0 +1,17 @@
+﻿using blendnet.common.dto.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blendnet.common.dto.Events
+{
+    /// <summary>
+    /// RetailerCreatedIntegrationEvent
+    /// </summary>
+    public class RetailerCreatedIntegrationEvent: IntegrationEvent
+    {
+
+    }
+}
---
+++ blendnet.retailer.listener/EventListener.cs
@@ -0,0 +1,62 @@
+﻿using blendnet.common.dto.Events;
+using blendnet.common.dto.Retailer;
+using blendnet.common.infrastructure;
+using blendnet.retailer.listener.IntegrationEventHandling;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace blendnet.retailer.listener
+{
+    public class EventListener: IHostedService
+    {
+        private readonly ILogger<EventListener> _logger;
+
+        private readonly IEventBus _eventBus;
+
+        private readonly RetailerAppSettings _appSettings;
+
+        public EventListener(ILogger<EventListener> logger,
+                                IEventBus eventBus,
+                                IOptionsMonitor<RetailerAppSettings> optionsMonitor)
+        {
+            _logger = logger;
+
+            _eventBus = eventBus;
+
+            _appSettings = optionsMonitor.CurrentValue;
+        }
+
+
+        /// <summary>
+        /// Start Async
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Starting Eventlistner of blendnet.retailer.listener");
+
+            _eventBus.Subscribe<RetailerCreatedIntegrationEvent, RetailerCreatedIntegrationEventHandler>();
+
+            await _eventBus.StartProcessing();
+
+            _logger.LogInformation("Subscribe complete by blendnet.retailer.listener");
+
+        }
+
+        /// <summary>
+        /// Stop Async
+        /// </summary>
+        /// <param name="cancellationToken"></param>
+        /// <returns></returns>
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Stopping Eventlistner of blendnet.retailer.listener");
+
+            return Task.CompletedTask;
+        }
+    }
+}
