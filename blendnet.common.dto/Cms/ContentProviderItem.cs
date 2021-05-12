1c39ead7bc800835712f3d11b0536aaf30c98471
Shwetha G <shwg@microsoft.com>
2021-05-12 19:31:17 +0530

    Adding missing file

---
+++ blendnet.common.dto/Cms/ContentProviderItem.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace blendnet.common.dto.Cms
+{
+    /// <summary>
+    /// Holds information about content provider id and name used by consumer app
+    /// </summary>
+    public class ContentProviderItem
+    {
+        /// <summary>
+        /// Content provider id
+        /// </summary>
+        public Guid ContentProviderId { get; set; }
+
+        /// <summary>
+        /// Content provider name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
