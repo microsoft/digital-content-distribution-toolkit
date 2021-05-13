df19a8f9b8c897159301dbca6b51c23a241f208e
hemantk <hemantk@microsoft.com>
2021-05-13 16:15:39 +0530

    Testing swagger with Ingress

--- blendnet.retailer.api/Startup.cs
+++ blendnet.retailer.api/Startup.cs
@@ -122,6 +122,8 @@ namespace blendnet.retailer.api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UsePathBase("/retailerapi");
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
