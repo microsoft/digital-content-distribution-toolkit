7dc8f174cadbfa7f705114429e75bfa0d7a3b9d6
Shwetha G <shwg@microsoft.com>
2021-06-03 10:34:33 +0530

    Base project set up

---
+++ blendnet.incentive.api/Controllers/IncentiveController.cs
@@ -0,0 +1,39 @@
+﻿using blendnet.common.dto.Incentive;
+using blendnet.incentive.repository.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Localization;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace blendnet.incentive.api.Controllers
+{
+    public class IncentiveController : ControllerBase
+    {
+        private readonly ILogger _logger;
+
+        private IIncentiveRepository _incentiveRepository;
+
+        private IncentiveAppSettings _incentiveAppSettings;
+
+        IStringLocalizer<SharedResource> _stringLocalizer;
+
+        public IncentiveController(IIncentiveRepository incentiveRepository,
+                                ILogger<IncentiveController> logger,
+                                IOptionsMonitor<IncentiveAppSettings> optionsMonitor,
+                                IStringLocalizer<SharedResource> stringLocalizer)
+        {
+            _incentiveRepository = incentiveRepository;
+
+            _logger = logger;
+
+            _incentiveAppSettings = optionsMonitor.CurrentValue;
+
+            _stringLocalizer = stringLocalizer;
+        }
+
+        #region Incentive management methods
+
+
+        #endregion
+    }
+}
