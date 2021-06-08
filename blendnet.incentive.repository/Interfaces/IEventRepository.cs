9278d7341466bafa09c24f2a9cc73ace5c36d903
Shwetha G <shwg@microsoft.com>
2021-06-08 14:26:03 +0530

    Added event repository

---
+++ blendnet.incentive.repository/Interfaces/IEventRepository.cs
@@ -0,0 +1,23 @@
+﻿using blendnet.common.dto.Incentive;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blendnet.incentive.repository.Interfaces
+{
+    public interface IEventRepository
+    {
+        /// <summary>
+        /// Stores the event in container
+        /// </summary>
+        /// <param name="eventItem"></param>
+        /// <returns></returns>
+        public Task<Guid> StoreEvent(Event eventItem);
+
+        public List<Event> GetEvents(string eventGeneratorId, Audience audience, DateTime? startDate, DateTime? endDate);
+
+        
+    }
+}
