9a2c136e819986dca4b1cf3641904e87e0d2c0fd
Sameer Agrawal <sameera@microsoft.com>
2021-06-10 08:40:34 +0000

    Merged PR 224: Create Retailer AppInsights event
    Related work items: #2563

---
+++ blendnet.common.infrastructure/Extensions/TelemetryClientExtensions.cs
@@ -0,0 +1,18 @@
+using blendnet.common.dto.AIEvents;
+using Microsoft.ApplicationInsights;
+
+namespace blendnet.common.infrastructure.Extensions
+{
+    public static class TelemetryClientExtensions
+    {
+        /// <summary>
+        /// Wrapper extension to log a BaseAI event
+        /// </summary>
+        /// <param name="telemetryClient">telemetry client</param>
+        /// <param name="aiEvent">event to be logged</param>
+        public static void TrackEvent(this TelemetryClient telemetryClient, BaseAIEvent aiEvent)
+        {
+            telemetryClient.TrackEvent(aiEvent.GetAIEventName(), aiEvent.ToDictionary());
+        }
+    }
+}
