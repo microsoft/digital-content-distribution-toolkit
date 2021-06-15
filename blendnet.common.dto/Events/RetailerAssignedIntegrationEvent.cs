01f37d7c8aa93a925e8f314ba4d78f346bb0688e
hemantk <hemantk@microsoft.com>
2021-06-15 12:19:17 +0530

    Added Retailer Assigned Integration Event

---
+++ blendnet.common.dto/Events/RetailerAssignedIntegrationEvent.cs
@@ -0,0 +1,18 @@
+﻿using blendnet.common.dto.Integration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using blendnet.common.dto.User;
+
+namespace blendnet.common.dto.Events
+{
+    /// <summary>
+    /// Event raised in retailer referral code assigned to user.
+    /// </summary>
+    public class RetailerAssignedIntegrationEvent : IntegrationEvent
+    {
+        public User.User User { get; set; }
+    }
+}
---
+++ blendnet.incentive.listener/IntegrationEventHandling/RetailerAssignedIntegrationEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using blendnet.common.dto.Events;
+using blendnet.common.infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace blendnet.incentive.listener.IntegrationEventHandling
+{
+    /// <summary>
+    /// Handles the retailed assigned to consumer
+    /// </summary>
+    public class RetailerAssignedIntegrationEventHandler : IIntegrationEventHandler<RetailerAssignedIntegrationEvent>
+    {
+        /// <summary>
+        /// Insert the incentive event
+        /// </summary>
+        /// <param name="integrationEvent"></param>
+        /// <returns></returns>
+        public Task Handle(RetailerAssignedIntegrationEvent integrationEvent)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
