98f1d9a054b1b1bffd20fe62ff937881e7af9d23
Shwetha G <shwg@microsoft.com>
2021-07-16 14:47:55 +0530

    Added browse apis for retailer

--- blendnet.incentive.api/Controllers/IncentiveBrowseController.cs
+++ blendnet.incentive.api/Controllers/IncentiveBrowseController.cs
@@ -1,6 +1,9 @@
 ﻿using AutoMapper;
+using blendnet.api.proxy.Retailer;
 using blendnet.common.dto;
 using blendnet.common.dto.Incentive;
+using blendnet.common.dto.Retailer;
+using blendnet.common.dto.User;
 using blendnet.common.infrastructure.Authentication;
 using blendnet.incentive.api.Model;
 using blendnet.incentive.repository.Interfaces;
@@ -20,7 +23,7 @@ namespace blendnet.incentive.api.Controllers
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiVersion("1.0")]
     [ApiController]
-    [AuthorizeRoles(KaizalaIdentityRoles.User)]
+    [AuthorizeRoles(KaizalaIdentityRoles.SuperAdmin, KaizalaIdentityRoles.User, KaizalaIdentityRoles.Retailer)]
     public class IncentiveBrowseController : ControllerBase
     {
         private IMapper _mapper;
@@ -33,9 +36,12 @@ namespace blendnet.incentive.api.Controllers
 
         IStringLocalizer<SharedResource> _stringLocalizer;
 
+        private RetailerProxy _retailerProxy;
+
         public IncentiveBrowseController(IIncentiveRepository incentiveRepository,
                                 ILogger<IncentiveController> logger,
                                 IMapper mapper,
+                                RetailerProxy retailerProxy,
                                 IOptionsMonitor<IncentiveAppSettings> optionsMonitor,
                                 IStringLocalizer<SharedResource> stringLocalizer)
         {
@@ -44,6 +50,8 @@ namespace blendnet.incentive.api.Controllers
             _logger = logger;
 
             _mapper = mapper;
+
+            _retailerProxy = retailerProxy;
 
             _incentiveAppSettings = optionsMonitor.CurrentValue;
 
@@ -58,16 +66,11 @@ namespace blendnet.incentive.api.Controllers
         /// </summary>
         /// <param name="planType"></param>
         /// <returns></returns>
-        [HttpGet("consumer/{planType}", Name = nameof(GetConsumerActivePlan))]
+        [HttpGet("consumer/active/{planType}", Name = nameof(GetConsumerActivePlan))]
         [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
+        [AuthorizeRoles(KaizalaIdentityRoles.SuperAdmin, KaizalaIdentityRoles.User)]
         public async Task<ActionResult<IncentivePlanDto>> GetConsumerActivePlan(PlanType planType)
         {
-            Audience audience = new Audience()
-            {
-                AudienceType = AudienceType.CONSUMER,
-                SubTypeName = ApplicationConstants.Common.CONSUMER
-            };
-
             IncentivePlan incentivePlan = await _incentiveRepository.GetCurrentConsumerActivePlan(planType);
 
             if (incentivePlan == null)
@@ -78,6 +81,111 @@ namespace blendnet.incentive.api.Controllers
             IncentivePlanDto incentivePlanDto = _mapper.Map<IncentivePlan, IncentivePlanDto>(incentivePlan);
 
             return Ok(incentivePlanDto);
+        }
+
+        /// <summary>
+        /// Returns retailer current active incentive plan with given plan type
+        /// </summary>
+        /// <param name="planType"></param>
+        /// <returns></returns>
+        [HttpGet("retailer/active/{retailerPartnerProvidedId}/{planType}/{subtypeName}", Name = nameof(GetRetailerActivePlan))]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
+        [AuthorizeRoles(KaizalaIdentityRoles.SuperAdmin, KaizalaIdentityRoles.Retailer)]
+        public async Task<ActionResult<IncentivePlanDto>> GetRetailerActivePlan(string retailerPartnerProvidedId, PlanType planType, string subtypeName)
+        {
+            RetailerDto retailer = await _retailerProxy.GetRetailerById(retailerPartnerProvidedId, subtypeName);
+
+            List<string> errorInfo = ValidateRetailer(retailer);
+
+            if (errorInfo.Count > 0)
+            {
+                return BadRequest(errorInfo);
+            }
+
+            IncentivePlan incentivePlan = await _incentiveRepository.GetCurrentRetailerActivePlan(planType, subtypeName);
+
+            if (incentivePlan == null)
+            {
+                return NotFound();
+            }
+
+            IncentivePlanDto incentivePlanDto = _mapper.Map<IncentivePlan, IncentivePlanDto>(incentivePlan);
+
+            return Ok(incentivePlanDto);
+        }
+
+        /// <summary>
+        /// Returns retailer incentive plans with given plan type
+        /// </summary>
+        /// <param name="planType"></param>
+        /// <returns></returns>
+        [HttpGet("retailer/all/{retailerPartnerProvidedId}/{planType}/{subtypeName}", Name = nameof(GetRetailerAllIncentivePlans))]
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Get))]
+        [AuthorizeRoles(KaizalaIdentityRoles.SuperAdmin, KaizalaIdentityRoles.Retailer)]
+        public async Task<ActionResult<List<IncentivePlanDto>>> GetRetailerAllIncentivePlans(string retailerPartnerProvidedId, PlanType planType, string subtypeName)
+        {
+            RetailerDto retailer = await _retailerProxy.GetRetailerById(retailerPartnerProvidedId, subtypeName);
+
+            List<string> errorInfo = ValidateRetailer(retailer);
+
+            if (errorInfo.Count > 0)
+            {
+                return BadRequest(errorInfo);
+            }
+
+            Audience audience = new Audience()
+            {
+                AudienceType = AudienceType.RETAILER,
+                SubTypeName = subtypeName
+            };
+
+            List<IncentivePlan> incentivePlans = await _incentiveRepository.GetIncentivePlans(audience, planType);
+
+            if (incentivePlans == null || incentivePlans.Count == 0)
+            {
+                return NotFound();
+            }
+
+            List<IncentivePlanDto> incentivePlanDto = _mapper.Map<List<IncentivePlan>, List<IncentivePlanDto>>(incentivePlans);
+
+            return Ok(incentivePlanDto);
+        }
+
+        #endregion
+
+        #region private methods
+        /// <summary>
+        /// Checks if retailer exists and if retailer id is same as current user id
+        /// </summary>
+        /// <param name="retailer"></param>
+        /// <param name="retailerPartnerProvidedId"></param>
+        /// <param name="partnerCode"></param>
+        /// <returns></returns>
+        private List<string> ValidateRetailer(RetailerDto retailer)
+        {
+            List<string> errorInfo = new List<string>();
+
+            if (retailer == null)
+            {
+                errorInfo.Add(_stringLocalizer["INC_ERR_0025"]);
+                return errorInfo;
+            }
+
+            if (User.IsInRole(KaizalaIdentityRoles.SuperAdmin))
+            {
+                // Retailer specific validation is not required for superadmin
+                return errorInfo;
+            }
+
+            Guid userId = UserClaimData.GetUserId(User.Claims);
+
+            if (!userId.Equals(retailer.Id))
+            {
+                errorInfo.Add(_stringLocalizer["INC_ERR_0026"]);
+                return errorInfo;
+            }
+
+            return errorInfo;
         }
 
         #endregion
