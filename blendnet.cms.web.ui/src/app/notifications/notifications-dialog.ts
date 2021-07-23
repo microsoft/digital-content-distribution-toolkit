1ea4689e4529c274c4174040716cc8cb2fb4cb03
Vikram Reddy <v-vjanagam@microsoft.com>
2021-07-23 05:18:08 +0000

    Merged PR 331: notifications UI redesign and minor fixes

--- blendnet.cms.web.ui/src/app/notifications/notifications-dialog.ts
+++ blendnet.cms.web.ui/src/app/notifications/notifications-dialog.ts
@@ -6,13 +6,19 @@ import { DialogData } from "../broadcast/broadcast.component";
 import { Time } from "@angular/common";
 import { SubscriptionService } from '../services/subscription.service';
 import { UploaderService } from "../services/uploader.service";
+import {MatChipInputEvent} from '@angular/material/chips';
+import {COMMA, ENTER} from '@angular/cdk/keycodes';
 
+export interface Fruit {
+  name: string;
+}
 
 @Component({
     selector: 'app-notifications-dialog',
     templateUrl: 'notifications-dialog.html',
     styleUrls: ['./notifications.component.css']
   })
+  
   export class NotificationsDialog implements OnInit {
     notif:string ="";
     expiresIn:string= "";
@@ -23,21 +29,24 @@ import { UploaderService } from "../services/uploader.service";
     minStart: Date;
     minTime: Time;
     @Output() onNotifCreate = new EventEmitter<any>();
-    fileUploadError: string ="";
+    // fileUploadError: string ="";
     file = File;
     progress: number;
     infoMessage: any;
     isUploading: boolean = false;
     subForm: FormGroup;
-    firstFormGroup: FormGroup;
-    secondFormGroup: FormGroup;
+    notifFormGroup: FormGroup;
+    // secondFormGroup: FormGroup;
     isEditable = false;
     @Input() scheduleChecked = true;
-
+    tags: Array<string> = [];
     imageUrl: string | ArrayBuffer = "https://th.bing.com/th/id/OIP.xtB199njiKybNeU8z0bSWAHaCy?pid=ImgDet&rs=1"
     //"https://th.bing.com/th/id/OIP.MRd8X-X-GRdY3tcOKTLDEwHaHa?pid=ImgDet&rs=1";
-    fileName: string = "No file selected";
-
+    // fileName: string = "No file selected";
+    selectable = true;
+    removable = true;
+    addOnBlur = true;
+    readonly separatorKeysCodes = [ENTER, COMMA] as const;
     constructor(
         public uploader: UploaderService,
         public dialogRef: MatDialogRef<NotificationsDialog>,
@@ -54,21 +63,19 @@ import { UploaderService } from "../services/uploader.service";
       }
   
     ngOnInit() {
-      this.subForm = new FormGroup({
+      this.notifFormGroup = new FormGroup({
         title :  new FormControl('', [Validators.required]),
-        text : new FormControl('')
-      }),
-      
-      this.firstFormGroup = this._formBuilder.group({
-        //firstCtrl: ['', Validators.required],
-        title :  new FormControl('', [Validators.required]),
-        text : new FormControl('')
+        body : new FormControl('', [Validators.required]),
+        attachmentUrl :  new FormControl('', [Validators.required]),
+        type :  new FormControl(0),
+        topic: new FormControl('', [Validators.required]),
+        tags: new FormControl(''),
       }),
 
-      this.secondFormGroup = this._formBuilder.group({
-        sendDate : new FormControl(null, [Validators.required]),
-        sendTime : new FormControl(null, [Validators.required])
-      }),
+      // this.secondFormGroup = this._formBuilder.group({
+      //   sendDate : new FormControl(null, [Validators.required]),
+      //   sendTime : new FormControl(null, [Validators.required])
+      // }),
 
       this.uploader.progressSource.subscribe(progress => {
         this.progress = progress;
@@ -82,33 +89,64 @@ import { UploaderService } from "../services/uploader.service";
   
     
     public errorHandling = (control: string, error: string) => {
-        return this.firstFormGroup.controls[control].hasError(error);
+        return this.notifFormGroup.controls[control].hasError(error);
       }
 
-    onChange(file: File) {
-      if (file) {
-        this.fileName = file.name;
-        this.file = File;
-  
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-  
-        reader.onload = event => {
-          this.imageUrl = reader.result;
-        };
+      add(event: MatChipInputEvent): void {
+        const value = (event.value || '').trim();
+    
+        // Add our fruit
+        if (value) {
+          this.tags.push(value);
+        }
+    
+        // Clear the input value
+        event.input.value='';
+        // this.notifFormGroup.value.push(value);
+        // this.courseIds.updateValueAndValidity();
+        console.log(this.tags);
       }
-    }
+    
+      remove(tag: string): void {
+        const index = this.tags.indexOf(tag);
+    
+        if (index >= 0) {
+          this.tags.splice(index, 1);
+        }
+      }
+
+      createNotif() {
+        console.log(
+        this.notifFormGroup.value);
+        Object.keys(this.notifFormGroup.value).forEach(data => {
+          console.log(data);
+        });
+      }
+
+    // onChange(file: File) {
+    //   if (file) {
+    //     this.fileName = file.name;
+    //     this.file = File;
   
-    onUpload(file: File) {
-      this.infoMessage = null;
-      this.progress = 0;
-      this.isUploading = true;
-      this.file = File;
+    //     const reader = new FileReader();
+    //     reader.readAsDataURL(file);
   
-      this.uploader.upload(file).subscribe(message => {
-        this.isUploading = false;
-        this.infoMessage = message;
-      });
-    }
+    //     reader.onload = event => {
+    //       this.imageUrl = reader.result;
+    //     };
+    //   }
+    // }
+  
+    // onUpload(file: File) {
+    //   this.infoMessage = null;
+    //   this.progress = 0;
+    //   this.isUploading = true;
+    //   this.file = File;
+  
+    //   this.uploader.upload(file).subscribe(message => {
+    //     this.isUploading = false;
+    //     this.infoMessage = message;
+    //   });
+    // }
    
   }
