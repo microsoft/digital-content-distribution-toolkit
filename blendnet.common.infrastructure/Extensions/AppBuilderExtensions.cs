2da0259badecdd7dd7749d1b82147c92f26c2182
Hemant Kathuria <hemantk@microsoft.com>
2022-02-23 10:13:44 +0000

    Merged PR 986: Return bad request in case of invalid continuation token
    Return bad request in case of invalid continuation token

--- blendnet.common.infrastructure/Extensions/AppBuilderExtensions.cs
+++ blendnet.common.infrastructure/Extensions/AppBuilderExtensions.cs
@@ -1,14 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.AspNetCore.Diagnostics;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 
 namespace blendnet.common.infrastructure.Extensions
@@ -22,13 +16,27 @@ namespace blendnet.common.infrastructure.Extensions
             {
                 var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
 
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                string result;
+
+                //not an ideal way but MalformedContinuationTokenException is private. hence added a check based on type name
+                if (exceptionHandlerPathFeature.Error != null &&
+                    exceptionHandlerPathFeature.Error.InnerException != null &&
+                    exceptionHandlerPathFeature.Error.InnerException.GetType().FullName == "Microsoft.Azure.Cosmos.Query.Core.Exceptions.MalformedContinuationTokenException")
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+                    result = JsonSerializer.Serialize(new { error = "Malformed Continuation Token" });
+                }
+                else
+                {
+                    context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+                    result = JsonSerializer.Serialize(new { error = HttpStatusCode.InternalServerError.ToString() });
+                }
 
                 context.Response.ContentType = "application/json";
 
                 logger.LogError(exceptionHandlerPathFeature.Error, exceptionHandlerPathFeature.Error.Message, exceptionHandlerPathFeature.Path);
-
-                var result = JsonSerializer.Serialize(new { error = HttpStatusCode.InternalServerError.ToString() });
 
                 await context.Response.WriteAsync(result);
             });
